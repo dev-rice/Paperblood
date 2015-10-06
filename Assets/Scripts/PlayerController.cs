73912afd044ca700d127ee17508792efe376a530
Trevor Westphal <trevorawestphal@gmail.com>
2015-10-05 22:37:07 -0600

    Added simple diablo-style movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float movement_sensitivity;
+
+	void Update () {
+		if(Input.GetButton("Fire1")){
+			Vector2 mousePosition = new Vector2(Input.mousePosition.x / Screen.width, Input.mousePosition.y / Screen.height);
+			Vector2 screenCenter = new Vector2(0.5f, 0.5f);
+			Vector2 movementVector = movement_sensitivity * (mousePosition - screenCenter) / 0.5f;
+
+			transform.Translate(movementVector.x, 0.0f, movementVector.y);
+		}
+	}
+}
