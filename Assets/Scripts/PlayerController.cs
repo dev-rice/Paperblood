de72c6128a8a8376e2f61077ea607817ba84f047
Trevor Westphal <trevorawestphal@gmail.com>
2015-10-06 09:25:46 -0600

    Diablo style controls finished - used left mouse button now.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,32 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float movement_sensitivity;
-
 	public float offset_theta;
 
+	private float cosTheta;
+	private float sinTheta;
+
+	void Start(){
+		float theta_rads = offset_theta * Mathf.PI / 180.0f;
+		cosTheta = Mathf.Cos(theta_rads);
+		sinTheta = Mathf.Sin(theta_rads);
+	}
+
 	void Update(){
-		if(Input.GetButton("Fire1")){
-			Vector2 screenMovement = new Vector2(
-				(Input.mousePosition.x / Screen.width - 0.5f) / 0.5f,
-				(Input.mousePosition.y / Screen.height - 0.5f) / 0.5f
+		// Left mouse button
+		if(Input.GetButton("Fire2")){
+			Vector2 movementVector = new Vector2(
+				((Input.mousePosition.x / Screen.width) - 0.5f) / 0.5f,
+				((Input.mousePosition.y / Screen.height) - 0.5f) / 0.5f
 			);
-			screenMovement *= movement_sensitivity;
 
-			// Clusterfuck right now, and also doesn't work
-			Vector3 gameMovement = new Vector3(
-				screenMovement.x * Mathf.Cos(offset_theta * Mathf.PI / 180.0f) + screenMovement.y * Mathf.Sin(offset_theta * Mathf.PI / 180.0f),
+			Vector3 something = new Vector3(
+				(movementVector.x * cosTheta) + (movementVector.y * sinTheta),
 				0.0f,
-				screenMovement.x * Mathf.Sin(offset_theta * Mathf.PI / 180.0f) + screenMovement.y * Mathf.Cos(offset_theta * Mathf.PI / 180.0f)
+				(movementVector.x * -1.0f * sinTheta) + (movementVector.y * cosTheta)
 			);
 
-			transform.position += gameMovement;
+			transform.position += something;
 		}
 	}
 }
