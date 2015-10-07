519ebd70a203af0c1fb39ec616b439499f9a8153
Trevor Westphal <trevorawestphal@gmail.com>
2015-10-06 21:26:31 -0600

    Movement working with physics!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,33 +3,40 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float movement_sensitivity;
+	public float speed;
 	public float offset_theta;
+	public float height_offset;
 
 	private float cosTheta;
 	private float sinTheta;
+	private Rigidbody rb;
 
 	void Start(){
+		// Precalculate the cos and sin of the world offset
 		float theta_rads = offset_theta * Mathf.PI / 180.0f;
 		cosTheta = Mathf.Cos(theta_rads);
 		sinTheta = Mathf.Sin(theta_rads);
+
+		rb = GetComponent<Rigidbody>();
 	}
 
-	void Update(){
-		// Left mouse button
+	void FixedUpdate(){
+
 		if(Input.GetButton("Fire2")){
-			Vector2 movementVector = new Vector2(
+			Vector2 screenMovement = new Vector2(
 				((Input.mousePosition.x / Screen.width) - 0.5f) / 0.5f,
 				((Input.mousePosition.y / Screen.height) - 0.5f) / 0.5f
 			);
 
-			Vector3 something = new Vector3(
-				(movementVector.x * cosTheta) + (movementVector.y * sinTheta),
+			Vector3 worldMovement = new Vector3(
+				(screenMovement.x * cosTheta) + (screenMovement.y * sinTheta),
 				0.0f,
-				(movementVector.x * -1.0f * sinTheta) + (movementVector.y * cosTheta)
+				(screenMovement.x * -1.0f * sinTheta) + (screenMovement.y * cosTheta)
 			);
 
-			transform.position += something;
+			transform.position += worldMovement * speed;
 		}
+
 	}
+
 }
